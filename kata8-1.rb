3bae395c1d3379648749a12ac19167df7a79c1dd
Artem Stepanchenko <astepanchenko1@gmail.com>
2024-08-30 09:12:00 +0300

    kata 8-1

---
+++ kata8-1.rb
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/wilson-primes
+
+def am_I_Wilson(n)
+    return false if n <= 1
+    
+    fact = 1
+    (1...n).each do |i|
+      fact *= i
+    end
+    
+    (fact + 1) % (n * n) == 0
+  end
