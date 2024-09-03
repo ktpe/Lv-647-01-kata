4c7bbd43d4cf5674611e3375feeade31b85d3265
Artem Stepanchenko <astepanchenko1@gmail.com>
2024-09-03 22:13:22 +0300

    Refactored kata 7

--- kata7.rb
+++ kata7.rb
@@ -1,8 +1,22 @@
 # https://www.codewars.com/kata/looking-for-a-benefactor
 
-def new_avg(arr, newavg)
-    s = newavg * (arr.length + 1)
-    last = (s - arr.sum).ceil
+
+# dons - масив поточних донатів [14, 30, 5, 7, 9, 11, 15]
+#
+# newavg — бажане середнє значення 
+# для всіх донатів після додавання нового.
+#
+def new_avg(dons, newavg)
+    
+    # кількість донатів з урахуванням нового
+    donations_count = dons.length + 1
+    
+    # загальна сума всіх донатів після 
+    # додавання нового
+    total_sum = donations_count * newavg
+    
+    expected_donation = (total_sum - dons.sum).ceil
+   
     raise 'Exception' if last < 0
-    last
+    expected_donation
 end
